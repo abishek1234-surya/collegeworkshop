65677ff185cb11f22e7c724082ce111df6330095
THAMILMANI VARATHARAJAN <v.kishoremani@gmail.com>
2023-08-05 13:00:55 +0530

    add StudentList

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -7,11 +7,20 @@ import java.util.List;
 
 public class StudentService {
     public static void main(String... args){
+        StudentService ss=new String.format();
+        ss.addStudent();
+        ss.getStudents();
 
     }
 
     public void addStudent(){
-
+      Student student=new Student();
+      List<Student> StudentList = new ArrayList<Student>();
+      Student.setStudentId("001");
+      Student.setname("name");
+      Student.setcourse("BCA");
+      Student.setStartDAte("21/07/2023");
+      Student.setEndDAte("21/07/2027");
     }
 
     public List<Student> getStudents(){
